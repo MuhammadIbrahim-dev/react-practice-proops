783b0439af4bac95c4c024c801282c98dffbc510
ibrahim-programming <muhammadibrahimmughal35@gmail.com>
2024-11-04 05:33:28 -0800

    react practice proops and bootsrap use

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+
+import { Children } from 'react';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import { blog } from './blog';
+import { Container } from 'react-bootstrap';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <Container>
+        <row>
+      {blog.map((v,i)=>{
+        return(
+        <ProductItem pitem ={v} key={i}/>
+        )
+      })}
+      </row>
+      </Container>
+    </>
+  )
+}
+export default App;
+
+export  function ProductItem({pitem}){
+  return (
+    <div>
+    <Card>
+      <Card.Body style={{display:'flex',width:'10rem'}}>
+        <Card.Title>{pitem.title}</Card.Title>
+        <Card.Text>
+          {pitem.body}
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
     </div>
-  );
+  )
 }
 
-export default App;
